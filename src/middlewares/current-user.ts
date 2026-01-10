7dae14f2a386e4a5e453b3ddd70aedcb4e6bc55d
Ajai Joseph <ajayjoseo8@gmail.com>
2026-01-10 21:56:38 +0530

    updates

--- src/middlewares/current-user.ts
+++ src/middlewares/current-user.ts
@@ -14,13 +14,14 @@ declare global{
 }
 export const currentUser=(req:Request,res:Response,next:NextFunction)=>{
     if(!(req as any)?.session?.jwt){
+        console.log(req)
         return next()
     }
     try{
         const payload=jwt.verify((req as any).session.jwt,process.env.JWT_KEY!) as UserPayload
         req.currentUser=payload
     }catch(err){
-
+    console.log(err)
     }
     next()
 }
